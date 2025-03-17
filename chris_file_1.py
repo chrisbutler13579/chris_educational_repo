5622020af94d42cadba66a8bf053e8b8c769e8de
Christopher Butler <christopher.butler@oldmissioncapital.com>
2025-03-17 11:45:39 -0400

    I added two new lines to practice how git works

--- chris_file_1.py
+++ chris_file_1.py
@@ -1,3 +1,4 @@
 # chris file 1:
-# this is just to learn about how the git stuff works 
+# this is just to learn about how the git stuff works
 x=10
+# here is a new line of code which I have just added
